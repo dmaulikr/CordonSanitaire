4e2e5e9587474950b3f480a18215fb64ed806134
Jonathan Bobrow <jbobrow@gmail.com>
2014-11-03 08:31:26 -0500

    adding a wireframe site to start prototyping the interaction online

---
+++ site/js/pubnub_helloworld.js
@@ -0,0 +1,40 @@
+ // Initialize with Publish & Subscribe Keys
+ 
+ var pubnub = PUBNUB.init({
+     publish_key: 'pub-c-f1d4a0b1-66e6-48ae-bd2b-72bcaac47884',
+     subscribe_key: 'pub-c-f1d4a0b1-66e6-48ae-bd2b-72bcaac47884'
+ });
+
+// get the time
+pubnub.time(
+	function(time){
+    	console.log(time)
+	}
+);
+
+// Subscribe to a channel
+ 
+pubnub.subscribe({
+	channel: 'my_channel',
+	message: function(m){console.log(m)}
+});
+
+ // Publish a welcome message
+ 
+ pubnub.publish({
+     channel: 'my_channel',        
+     message: 'Hello from the PubNub Javascript SDK'
+ });
+
+ // Get List of Occupants and Occupancy Count.
+ 
+ pubnub.here_now({
+     channel : 'my_channel',
+     callback : function(m){console.log(m)}
+ });
+
+  pubnub.subscribe({
+     channel: "my_channel",
+     presence: function(m){console.log(m)},
+     message: function(m){console.log(m)}
+ });
---
+++ site/js/timer.js
@@ -0,0 +1,32 @@
+var seconds = 59;
+var hundredths = 100;
+function timePassed() {
+    var minutes = Math.round((seconds - 30)/60);
+    var remainingSeconds = seconds % 60;
+    var remainingHundredths = hundredths % 100;
+
+    if (remainingHundredths < 10) {
+    	remainingHundredths = "0" + remainingHundredths;
+    }
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;  
+    }
+    if (minutes < 10) {
+    	minutes = "0" + minutes;
+    }
+
+    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds + ":" + remainingHundredths;
+    
+    if(hundredths == 0) {
+    	hundredths = 100;
+    	seconds--;
+    }
+    if (seconds == 0) {
+        clearInterval(timePassed);
+        document.getElementById('countdown').innerHTML = "00:00:00 -- Patient Zero Contained";
+    } else {
+        hundredths--;
+    }
+}
+ 
+var countdownTimer = setInterval('timePassed()', 10);
