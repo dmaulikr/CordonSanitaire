caf99267432d7df010b34831e0cc420b15a9aa77
Jonathan Bobrow <jbobrow@gmail.com>
2014-12-30 08:27:35 -0500

    Simple page to send the EZ text with a button press

---
+++ site/control/index.php
@@ -0,0 +1,17 @@
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<title>Test EZ Text</title>
+		<meta name="description" content="">
+		<meta name="keywords" content="" />
+		<meta name="author" content="">
+		<!-- !CSS -->
+	</head>
+	<body>		
+		<!--<button onclick="makeCorsRequest()">Make CORS Request</button>-->
+		<button onclick="sendTextMessage()">Send Text Message</button>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
+		<script type="text/javascript" src="../js/eztext.js"></script>
+	</body>
+</html>
