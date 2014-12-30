2459da68bb5eb788b47ab0bb6de93b110102b1de
Jonathan Bobrow <jbobrow@gmail.com>
2014-12-30 08:27:55 -0500

    Simple javascript to send the EZ text with a button press

--- site/js/eztext.js
+++ site/js/eztext.js
@@ -1,35 +1,46 @@
 // curl -d 'User=winnie&Password=the-pooh&PhoneNumbers[]=2123456785&PhoneNumbers[]=2123456786&PhoneNumbers[]=2123456787&PhoneNumbers[]=2123456788&Groups[]=honey lovers&Subject=From Winnie&Message=I am a Bear of Very Little Brain, and long words bother me&StampToSend=1305582245' https://app.eztexting.com/sending/messages?format=xml
 
 function sendCurlRequest(){
+	
     var json_data = {
                 "User": "jbobrow",
-                "Password": "",
+                "Password": "dmiHQzPp6Kxt3q",
                 "Subject":"From JB",
 				"Message":"Testing EZ Text API",
-				"StampToSend":"05-16-2011 5:44 PM",
 				"PhoneNumbers":[
-					"8186207518",
 					"8186207518" 
 					]};
+					
     $.ajax({
-         cache : false,       
-     type: 'POST',
-     crossDomain:true,
-     url: 'https://app.eztexting.com/sending/messages?format=json',
-     data:json_data,
-     //dataType: "jsonp",
-     contentType:"application/jsonp",
-     success: function(data){
-            alert(data);
-        var pubResults = data;       
-     },
-     error: function(data){
-       alert("ERROR RESPONSE FROM DRUID SERVER : "+JSON.stringify(data));
-     },
-     complete: function(data){
-        console.log("call completed");
-     }
-   });
+	    cache : false,       
+	    type: 'POST',
+	    crossDomain:true,
+	    url: 'https://app.eztexting.com/sending/messages?format=json',
+	    data:json_data,
+// 	    dataType: "jsonp",
+	    contentType:"application/json",
+	    success: function(data){
+	            alert(data);
+	        var pubResults = data;       
+	    },
+	    error: function(data){
+	    	alert("ERROR RESPONSE FROM DRUID SERVER : "+JSON.stringify(data));
+	    },
+	    complete: function(data){
+	        console.log("call completed");
+	    }
+    });
 }
 
-sendCurlRequest();
+// Make the actual CORS request.
+// http://stackoverflow.com/questions/20035101/no-access-control-allow-origin-header-is-present-on-the-requested-resource
+// https://developer.chrome.com/extensions/xhr
+// http://www.html5rocks.com/en/tutorials/cors/#toc-adding-cors-support-to-the-server
+// Check this out later. Today, we hack :)
+
+var sendTextMessage = function() {
+	// simply send to the php page that successfully sends the text message
+	var url = "http://playful.jonathanbobrow.com/prototypes/cordonsans/control/sendSMS/"
+	var win = window.open(url, '_blank');
+	//sendCurlRequest();
+};
