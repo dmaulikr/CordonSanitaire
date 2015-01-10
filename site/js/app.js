8cb9e4884951cb20dcdb77e0694d17ab30413feb
Jonathan Bobrow <jbobrow@gmail.com>
2015-01-10 16:56:59 -0500

    send alert if user navigates away from the page

--- site/js/app.js
+++ site/js/app.js
@@ -14,6 +14,46 @@ if (!window.chrome) {
 	window.location = "http://playful.jonathanbobrow.com/prototypes/cordonsans/unsupported/"
 }
 
+var isWindowInFocus = true;
+
+// NOTIFY USER WITH AN ALERT IF THE GAME IS STARTING AND THEY NAVIGATE AWAY
+// Set the name of the hidden property and the change event for visibility
+var hidden, visibilityChange; 
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.mozHidden !== "undefined") {
+  hidden = "mozHidden";
+  visibilityChange = "mozvisibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+ 
+// If the page is hidden, pause the video;
+// if the page is shown, play the video
+function handleVisibilityChange() {
+	if (document[hidden]) {
+		isWindowInFocus = false;
+		console.log("window out of focus");
+	} else {
+		isWindowInFocus = true;
+		console.log("window in focus");
+	}
+}
+
+// Warn if the browser doesn't support addEventListener or the Page Visibility API
+if (typeof document.addEventListener === "undefined" || 
+  typeof document[hidden] === "undefined") {
+  alert("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+} else {
+  // Handle page visibility change   
+  document.addEventListener(visibilityChange, handleVisibilityChange, false);
+}    
+
 
 
 var _channel = 'production';	// Dev Channel vs. Production Channel
