7a86de722ebcac0a8266658271419f171b13799a
Jonathan Bobrow <jbobrow@gmail.com>
2015-03-13 05:30:54 -0400

    change the view controllers to inherit from view controllers and table view controllers

--- iOS/Cordon Sanitaire/Cordon Sanitaire/IntroViewController.swift
+++ iOS/Cordon Sanitaire/Cordon Sanitaire/IntroViewController.swift
@@ -1,9 +1,36 @@
+
 //
 //  IntroViewController.swift
 //  Cordon Sanitaire
 //
-//  Created by Jonathan Bobrow on 3/2/15.
+//  Created by Jonathan Bobrow on 3/13/15.
 //  Copyright (c) 2015 Playful Systems. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS/Cordon Sanitaire/Cordon Sanitaire/MapViewController.swift
+++ iOS/Cordon Sanitaire/Cordon Sanitaire/MapViewController.swift
@@ -2,26 +2,34 @@
 //  MapViewController.swift
 //  Cordon Sanitaire
 //
-//  Created by Jonathan Bobrow on 3/2/15.
+//  Created by Jonathan Bobrow on 3/13/15.
 //  Copyright (c) 2015 Playful Systems. All rights reserved.
 //
 
-import Foundation
-import MapKit
 import UIKit
 
-class MapViewController: UIViewController, MKMapViewDelegate {
-    
+class MapViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
-    
-    func createMap() {
-        
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
