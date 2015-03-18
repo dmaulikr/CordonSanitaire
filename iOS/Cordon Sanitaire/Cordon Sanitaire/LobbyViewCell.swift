6008949c86ce92949971022c92a675e6ef307580
Jonathan Bobrow <jbobrow@gmail.com>
2015-03-18 04:59:40 -0400

    adding a custom table view controller for the lobby view

---
+++ iOS/Cordon Sanitaire/Cordon Sanitaire/LobbyViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LobbyViewCell.swift
+//  Cordon Sanitaire
+//
+//  Created by Jonathan Bobrow on 3/18/15.
+//  Copyright (c) 2015 Playful Systems. All rights reserved.
+//
+
+import UIKit
+
+class LobbyViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
