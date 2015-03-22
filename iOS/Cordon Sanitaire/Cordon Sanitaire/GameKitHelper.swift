ad7f038f4f63a6c08149bc8fbb998107a5457cd0
Jonathan Bobrow <jbobrow@gmail.com>
2015-03-21 20:24:22 -0400

    add a game kit helper to handle game kit needs

---
+++ iOS/Cordon Sanitaire/Cordon Sanitaire/GameKitHelper.swift
@@ -0,0 +1,47 @@
+//
+//  GameKitHelper.swift
+//  Cordon Sanitaire
+//
+//  Created by Jonathan Bobrow on 3/21/15.
+//  Copyright (c) 2015 Playful Systems. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+private let _SingletonSharedInstance = GameKitHelper()
+
+class GameKitHelper: NSObject {
+
+    class var singleton: GameKitHelper! {
+        return _SingletonSharedInstance
+    }
+    
+    var enableGameCenter: Bool!
+    
+    override init(){
+        super.init()
+        // code for game kit helper
+        
+        enableGameCenter = true
+    }
+    
+    func authenticateLocalPlayer() {
+        
+        var localPlayer = GKLocalPlayer.localPlayer()
+        
+        // look at local player information
+        println(localPlayer.debugDescription)
+        
+        localPlayer.authenticateHandler = {(viewController : UIViewController!, error : NSError!) -> Void in
+            
+            if ((viewController) != nil) {
+                //self.presentViewController(viewController, animated: true, completion: nil)
+            }else{
+                println((GKLocalPlayer.localPlayer().authenticated))
+                
+            }
+        }
+    }
+    
+}
