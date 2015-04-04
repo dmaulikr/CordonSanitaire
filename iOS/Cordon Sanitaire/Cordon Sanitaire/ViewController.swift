e7d921c3dfbb96689cc23e9b436fa46a7d7012f4
Jonathan Bobrow <jbobrow@gmail.com>
2015-04-03 20:07:50 -0400

    comment out the timer logging and change start game in main view controller

--- iOS/Cordon Sanitaire/Cordon Sanitaire/ViewController.swift
+++ iOS/Cordon Sanitaire/Cordon Sanitaire/ViewController.swift
@@ -58,7 +58,7 @@ class ViewController: UIViewController, GameDelegate {
        
         if(sender.titleLabel?.text == "PLAY"){
             // display the map view
-            createMapView()
+            Lobby.singleton.startGame()
         }
         else if(sender.titleLabel?.text == "INTRO") {
             // display the map view
