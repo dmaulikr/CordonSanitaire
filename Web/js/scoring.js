e01a35469b739f71cb6be55738bdd4e98284445d
mingem <mingwang@mit.edu>
2015-08-17 13:23:28 -0400

    Fleshed out functions. Added:getScore, getStats, calculate/updateScore, resetScore.

--- Web/js/scoring.js
+++ Web/js/scoring.js
@@ -1,32 +1,136 @@
-//creates a score object that is specific to each player and is updated after each move.
+//creates a score object that is specific to each player and should be updated after each move.
 var Score = function(id){
     this.id = id;
     //check for changes in user state for score updates
     this.status = getType(User.getPersonById(id));
     this.pZeroStatus = isPatientZeroContained();
+    this.casualties = countCasualties();
+    this.isGameOver = false;
+    
     //score
     this.score = 0;
-    //checks upon first action if joined in first 3
+    //in gmaps.js, updates the scores of first 3 individuals who join
+    this.joinFirst = false; //+10
+    this.joinSecond = false; //+5
+    this.joinThird = false; //+1
+    //updating numbers
+    this.peopleTrapped = 0; //-1
+    this.peopleSaved = 0; //+1
+
+    this.timesJoined = 0;
+    this.timesReleased = 0;
+    this.timesTrapped = 0;
+    //this.timesShouted = 0;
+
+    this.trappedPZero = 0; //+10
+    this.releasedPZero = 0; //-10
+
+    //Add something for forming/breaking the quarantine? +-5
+    //Add something for emoji use/shout? but don't want to promote spamming
+};
+
+//Uses number properties to update this.score
+Score.prototype.updateScore = function(){
+    //does this work asynchronously?
+    this.updateAction();
+    this.updatePeople();
+
+    this.score = this.calculateScore();
+};
+
+//calculates user score based on numbers
+Score.prototype.calculateScore = function(){
+    var joinBonus = 0;
+    if (this.joinFirst == true){
+        joinBonus = 10;
+    }
+    else if (this.joinSecond == true){
+        joinBonus = 5;
+    }
+    else if (this.joinThird == true){
+        joinBonus = 1;
+    }
+
+    var endBonus = 0;    //NOTE: not implemented yet: if game is over and P0 is trapped, add 100.
+    var calScore = this.peopleSaved - this.peopleTrapped + 10*this.trappedPZero - 10*this.releasedPZero + joinBonus + endBonus;
+    return calScore;
+};
+
+//Updates stats involving status of the user (ie joined, released, shouted)
+Score.prototype.updateAction = function(){
+    var newStatus = getType(User.getPersonByID(id));
+    if (newStatus == "ACTIVE"){
+        this.timesJoined +=1;
+    }
+    if (newStatus == "PASSIVE"){
+        this.timesReleased +=1;
+    }
+    if (newStatus == "TRAPPED"){
+        this.timesTrapped +=1;
+    }
+};
+
+//Updates people trapped, saved, and pzero
+Score.prototype.updatePeople = function(){
+    var newCasualties = countCasualties();
+    var diff = this.casualties - newCasualties;
+    if (diff > 0){
+        this.peopleTrapped += diff;
+    }
+    else {
+        this.peopleSaved -= diff;
+    }
+
+    //checks for changes in patient zero status
+    var newPZeroStatus = isPatientZeroContained();
+    if (this.pZeroStatus != newPZeroStatus){
+        if (newPZeroStatus == true){
+            this.trappedPZero +=1;
+        }
+        else {
+            this.releasedPZero +=1;
+        }
+    }
+};
+
+//Sets all values to zero, and states to current
+Score.prototype.resetScore = function(){
+    this.status = getType(User.getPersonById(id));
+    this.pZeroStatus = isPatientZeroContained();
+    this.casualties = countCasualties();
+
+    this.score = 0;
+
     this.joinFirst = false;
     this.joinSecond = false;
     this.joinThird = false;
-    //updating numbers
+
     this.peopleTrapped = 0;
     this.peopleSaved = 0;
+
     this.timesJoined = 0;
     this.timesReleased = 0;
-    this.timesShouted = 0;
+    //this.timesShouted = 0;
+
     this.trappedPZero = 0;
     this.releasedPZero = 0;
 };
 
-Score.prototype.updateScore = function(){
-    //checks to see if user was among first 3 to join, updates boolean values accordingly
-    this.didJoinFirst();
-    var newStatus = getType(User.getPersonById(id));
-
-
+//returns User score
+Score.prototype.getScore = function(){
+    return this.score;
 };
+
+//returns User's actions.
+Score.prototype.getStats = function(){
+    var stats = [
+        this.timesJoined,
+        this.timesReleased,
+        this.timesTrapped
+    ];
+    return stats;
+};
+
 
 function countCasualties() {
     var count = 0;
@@ -80,20 +184,3 @@ function isPatientZeroContained() {
 
     return _patientZeroContained
 }
-
-////check status of patient zero
-//if (_patientZeroContained)
-//    document.getElementById('patient_status').innerHTML = 'contained';
-//else
-//    document.getElementById('patient_status').innerHTML = 'not contained';
-//
-//// update count of casualties
-//document.getElementById('casualty_count').innerHTML = countCasualties();
-//
-//// calculate the sq mi of quarantine...
-//document.getElementById('area_quarantined').innerHTML = getAreaQuarantined();
-//
-//// update count of people quarantining
-//document.getElementById('num_active').innerHTML = countActivePeople();
-//
-//// update score
